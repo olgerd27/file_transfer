e3411480e13b8e0adc0ed2f0a2a8dee0751552d5
Oleh Matiiuk <olgerd27@gmail.com>
2024-01-12 14:24:01 +0200

    Moved the memory allocation for the error messages at the beginning of upload_file_1_svc()
    Also, changed the error numbers on a server side

--- server/prg_serv.c
+++ server/prg_serv.c
@@ -9,53 +9,43 @@
 extern int errno; // is global, defined in the system's standard C library
 
 /*
- * Memory allocation to store the error message
- */
-void malloc_errmsg(char **errmsg)
-{
-  if (*errmsg == NULL)
-    *errmsg = (char*)malloc(SIZE_ERRMSG);
-}
-
-/*
- * Reset the error information that remained from the previous call
- * when error has appeared
+ * Reset/init the error information
  */
 void reset_err(errinf *err)
 {
-  if (errno) errno = 0;
+  // allocate the memory for error messages
+  if (!err->errinf_u.msg)
+    err->errinf_u.msg = (char*)malloc(SIZE_ERRMSG);
+
+  // reset the error information that is remained from the previous case of an error
   if (err->num) {
     err->num = 0;
     if (err->errinf_u.msg)
       memset(err->errinf_u.msg, 0, SIZE_ERRMSG);
   }
+
+  // reset the system error number
+  if (errno) errno = 0;
 }
 
 errinf * upload_file_1_svc(file *file_upld, struct svc_req *)
 {
   static errinf res_err; /* must be static */
   printf("[main] 0\n");
-  printf("errno=%d  res_err: num=%d  msg(%p):\n%s\n", 
-         errno, res_err.num, (void*)res_err.errinf_u.msg, res_err.errinf_u.msg);
 
   // Reset the results of the previous call
   reset_err(&res_err);
   printf("[main] 1\n");
-  printf("errno=%d  res_err: num=%d  msg(%p):\n%s\n", 
-         errno, res_err.num, (void*)res_err.errinf_u.msg, res_err.errinf_u.msg);
 
   // Check the file existence
   // TODO: if file exists provide a choice to the client what to do next: 
   // Specify different name, Overwrite, Cancel
   if (access(file_upld->name, F_OK) == 0) {
-    res_err.num = 1;
-    malloc_errmsg(&res_err.errinf_u.msg);
+    res_err.num = 50;
     sprintf(res_err.errinf_u.msg, 
             "The specified file '%s' already exists.\n"
             "Please choose a different name to save a file.", file_upld->name); 
-    printf("[main 1.1]\n");
-    printf("errno=%d  res_err: num=%d  msg(%p):\n%s\n", 
-           errno, res_err.num, (void*)res_err.errinf_u.msg, res_err.errinf_u.msg);
+    printf("[main 1.1] ERROR #%i\n", res_err.num);
     return &res_err;
   }
   printf("[main] 2\n");
@@ -63,15 +53,12 @@ errinf * upload_file_1_svc(file *file_upld, struct svc_req *)
   // Open the file
   FILE *hfile = fopen(file_upld->name, "wb");
   if (hfile == NULL) {
-    res_err.num = 2;
-    malloc_errmsg(&res_err.errinf_u.msg);
+    res_err.num = 51;
     sprintf(res_err.errinf_u.msg, 
             "Cannot open the file: '%s' in the write mode.\n"
-            "System error #%i message:\n%s.",
+            "System error #%i message:\n%s",
             file_upld->name, errno, strerror(errno)); 
-    printf("[main 2.1]\n");
-    printf("errno=%d  res_err: num=%d  msg(%p):\n%s\n", 
-           errno, res_err.num, (void*)res_err.errinf_u.msg, res_err.errinf_u.msg);
+    printf("[main 2.1] ERROR #%i\n", res_err.num);
     return &res_err;
   }
   printf("[main] 3\n");
@@ -79,15 +66,12 @@ errinf * upload_file_1_svc(file *file_upld, struct svc_req *)
   // Write to the file
   fwrite(file_upld->cont.t_flcont_val, 1, file_upld->cont.t_flcont_len, hfile);
   if (ferror(hfile)) {
-    res_err.num = 3;
-    malloc_errmsg(&res_err.errinf_u.msg);
+    res_err.num = 52;
     sprintf(res_err.errinf_u.msg, 
             "Cannot write to the file: '%s'.\n"
-            "System error #%i message:\n%s.", 
+            "System error #%i message:\n%s", 
             file_upld->name, errno, strerror(errno));
-    printf("[main 3.1]\n");
-    printf("errno=%d  res_err: num=%d  msg(%p):\n%s\n", 
-           errno, res_err.num, (void*)res_err.errinf_u.msg, res_err.errinf_u.msg);
+    printf("[main 3.1] ERROR #%i\n", res_err.num);
     fclose(hfile);
     return &res_err;
   }
